7b7be99eca8058275851a80c240e597a83e1f11c
Nishant Rajput <81091584+nishantrajputnr@users.noreply.github.com>
2022-02-08 15:50:33 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,14 +1,1 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 
-# Create your views here.
-def hello(request):
-    if request.method=="GET":
-        a = request.GET['first']
-        b = request.GET["second"]
-        a=int(a)
-        b=int(b)
-        return HttpResponse(a+b)
-    return HttpResponse("asdfasdf")
-def fun1(request):
-    return render(request,"index.html")
