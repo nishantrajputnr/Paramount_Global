10e904b16cc26a05d3016beddef30fb63e221d30
Chirag Shangle <chiragshangle15@gmail.com>
2022-02-15 16:39:03 +0530

    name changed to aboota

--- aboota/asgi.py
+++ aboota/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Paramount_Global project.
+ASGI config for aboota project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Paramount_Global.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aboota.settings')
 
 application = get_asgi_application()
