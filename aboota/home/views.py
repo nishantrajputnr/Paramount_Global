e32dbab8af88753fc1b56bb6af38a70849eb326f
Ladycoderr <shrutimishra2884@gmail.com>
2022-02-19 14:53:08 +0530

    changes

--- aboota/home/views.py
+++ aboota/home/views.py
@@ -13,7 +13,7 @@ def trading(request):
     return render(request,"trading.html")     
 def terminal(request):
     return render(request,"terminal.html")     
-def referal(request):
-    return render(request,"referal.html")     
+def referral(request):
+    return render(request,"referral.html")     
 def contact(request):
     return render(request,"contact.html")     
